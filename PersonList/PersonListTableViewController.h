6332dbbda448893c712cfa261a939ac3f2cf0bf0
James Carlson <jamesrcarlson@gmail.com>
2015-07-01 20:38:40 -0600

    prepping table with data
    not able to view data

---
+++ PersonList/PersonListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonListTableViewController.h
+//  PersonList
+//
+//  Created by James Carlson on 7/1/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonListTableViewController : UITableViewController
+
+@end
